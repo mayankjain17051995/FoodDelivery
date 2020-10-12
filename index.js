e2cb9d860741aaa5f12bfef6534113ab93304f76
mayankjain1705 <mayankjain1705@gmail.com>
2020-10-12 19:14:33 +0530

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+app.use(bodyParser.urlencoded({ 
+    extended:true
+})); 
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 })
 
-app.get("/api/itemData", (req, res) => {
-    res.send(importData);
-})
+app.post("/", function(req, res) { 
+    var username = String(req.body.username); 
+    var password = String(req.body.password); 
+      
+    if (username == "mayankJain" && password == "India@2020") {
+        res.send(importData); 
+    } else {
+        res.send("You are not authorized to access the Din Dinn API");
+    }
+      
+  }); 
 
 app.listen(port, () => {
     console.log('App is listening on port');
